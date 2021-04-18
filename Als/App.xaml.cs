4daeae81883234ae63edb7c6c2d5d847801c4c9d
Siarhai Alenichau <als.at@hotmail.com>
2021-04-18 15:02:38 +0200

    DependencyInjection has been disposed

--- Als/App.xaml.cs
+++ Als/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,45 @@ namespace Als
     /// </summary>
     public partial class App : Application
     {
+
+        //Working Host
+        private static IHost _Host;
+        public static IHost Host => _Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+
+        //Posibility of Access to Services
+        public static IServiceProvider Services => Host.Services;
+
+
+        //Start the Host
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var host = Host;
+            await host.StartAsync().ConfigureAwait(false); //ConfigureAwait(false) is neccessary
+        }
+
+
+        //Close the Host
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _Host = null;
+        }
+
+
+        //Method for registration of the Services
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        { }
+            //=> services
+            //.AddDataBase(host.Configuration.GetSection("DataBase"))
+            //.AddServices()
+            //.AddViewModels()
+            //;
     }
 }
---
+++ Als/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Als
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)  //Make a Host via Class Host
+            //.ConfigureAppConfiguration((host, cfg) => cfg  //Make a configuration of the Application via Lambda
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true) //Type of the configuration file
+            //)
+            .ConfigureServices(App.ConfigureServices); //Method for Services Confoguration. But we make his at Class App          
+    }
+}
