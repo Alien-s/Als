53e748d3cf396aed82ab03dd2561839c487ce142
Siarhai Alenichau <als.at@hotmail.com>
2021-04-18 16:20:24 +0200

    DbRegistrator has been added

---
+++ Als/Data/DbRegistrator.cs
@@ -0,0 +1,31 @@
+﻿using Als.MDB.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Als.Data
+{
+    public static class DbRegistrator
+    {
+        public static IServiceCollection AddDataBase(this IServiceCollection services, IConfiguration Configuration) => services
+            .AddDbContext<AlsDB>(opt =>
+            {
+                var type = Configuration["Type"];
+                switch (type)
+                {
+                    case null: throw new InvalidOperationException("Unknow the DataBase type");
+
+                    default: throw new InvalidOperationException($"Connection type {type} is not supported");
+
+                    case "MSSQL":
+                    case "AdditionalConnection":
+                        opt.UseSqlServer(Configuration.GetConnectionString(type));
+                        //Can use also: UseSQLite, UseInMemoryDatabase ...
+                        break;
+                }
+            })
+            //.AddRepositoriesInDB()
+            ;
+    }
+}
