a053658f9ebe32bf2cf5f5fbe7dd1cc8c2ec31d2
Siarhai Alenichau <als.at@hotmail.com>
2021-04-18 16:14:59 +0200

    Appsettings.json file has been created

--- Als.MDB/Context/AlsDB.cs
+++ Als.MDB/Context/AlsDB.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Als.MDB.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Als.MDB.Context
 {
-    public class AlsDB
+    public class AlsDB : DbContext
     {
+        //Structure of DataBase
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Position> Positions { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+
+        //Contructor
+        public AlsDB(DbContextOptions<AlsDB> options) : base(options) { }
     }
 }
