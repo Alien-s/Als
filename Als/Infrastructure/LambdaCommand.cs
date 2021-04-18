cbc3a7950f62f0f145764554e022c42bc26ed59c
Siarhai Alenichau <als.at@hotmail.com>
2021-04-18 14:23:27 +0200

    Mvvm has been added

---
+++ Als/Infrastructure/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using Als.Infrastructure.Commands.BaseCommand;
+using System;
+
+namespace Als.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => execute(parameter);
+    }
+}
